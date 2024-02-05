e67b03ad1fda7d5f1ecbbafe2440c13efe88aa6c
BaGorK <edmealemkassahun@gmail.com>
2024-02-05 13:01:22 +0300

    modified

--- src/Components/PizzaList.js
+++ src/Components/PizzaList.js
@@ -1,13 +1,13 @@
 const PizzaList = ({ pizzaItem }) => {
-  const isSoldOut = pizzaItem.isSoldOut;
+  const isSoldOut = pizzaItem.soldOut;
 
   return (
-    <li className='pizza'>
+    <li className={`pizza ${isSoldOut ? 'sold-out' : ''}`}>
       <img src='../pizzas/focaccia.jpg' alt='focaccia' />
       <div>
         <h3>{pizzaItem.name}</h3>
         <p>{pizzaItem.ingredients}</p>
-        {!isSoldOut ? <span>sold out</span> : <span>pizzaItem.price</span>}
+        {!pizzaItem.soldOut ? <span>{pizzaItem.price}</span> : <span>sold out</span>}
       </div>
     </li>
   );
